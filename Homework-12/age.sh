35543bf14b65faea182f7c57e7c53f7bfe73b91e
Viktoriya88Lesheva <Viktoriyalesheva88@gmail.com>
2025-03-04 22:36:02 +0000

    Create HW-12

---
+++ Homework-12/age.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+read -p "Enter your year of birth: " year
+
+echo "In 2025 you will be $((2025-$year)) years old"
+echo "In 2020 you was $((2020-$year)) years old"
+echo "In 2030 you will be $((2030-$year)) years old"
